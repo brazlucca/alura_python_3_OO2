f7255d085721396dc2b9a7f61aaf6ca192b0ecab
brazlucca <brazlucca@gmail.com>
2022-02-02 20:48:06 -0300

    atributos privados, properties para acesso ao atributo

--- modelo.py
+++ modelo.py
@@ -1,18 +1,60 @@
 
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
+        self.__likes = 0
+
+    def dar_like(self):
+        self.likes += 1
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duracao: {vingadores.duracao}')
+vingadores.dar_like()
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duracao: {vingadores.duracao} - Likes: {vingadores.likes}')
 
-house_of_cards = Serie('house of cards', 2018, 2)
-print(f'Nome: {house_of_cards.nome} - Ano: {house_of_cards.ano} - Temporadas: {house_of_cards.temporadas}')
+house_of_cards = Serie('house of cards', 2018, 3)
+house_of_cards.nome = 'sons of anarchy'
+house_of_cards.dar_like()
+house_of_cards.dar_like()
+print(f'Nome: {house_of_cards.nome} - Ano: {house_of_cards.ano} - Temporadas: {house_of_cards.temporadas} - Likes: {house_of_cards.likes}')
+
+
+
